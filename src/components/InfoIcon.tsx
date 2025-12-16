09b89fe63916970d5257bc2737225867e7ff734e
Jackson Bickler <jbickler4@gmail.com>
2025-12-16 12:49:03 -0700

    Force new deployment - fix React import issue

--- src/components/InfoIcon.tsx
+++ src/components/InfoIcon.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './InfoIcon.css'
 
+// InfoIcon component - displays tooltip with page information
 interface InfoIconProps {
   content: string
   title?: string
